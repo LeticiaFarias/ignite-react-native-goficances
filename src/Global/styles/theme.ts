21ed7f25e4a1b95c943f1161d878d236fac2a03a
leticiafarias <leticia_farias@alu.ufc.br)>
2022-01-23 20:02:42 -0300

    :lipstick: Add Styled components
    Add styled components

---
+++ src/Global/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    primary: '#7159c1',
+    secondary: '#ff872c',
+    secondaryLight: 'rgba(255, 135, 44, 0.3)',
+    success: '#00c853',
+    successLight: 'rgba(18, 164, 84, 0.5)',
+    attention: '#e83f58',
+    attentionLight: 'rgba(232, 63, 88, 0.5)',
+    shape: '#ffffff',
+    title: '#363f5f',
+    text: '#969cb2',
+    background: '#f0f2f5'
+  },
+}
---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #7159c1;
+`;
+
+export const Title = styled.Text`
+  font-size: 26px;
+  color: #fff;
+  font-weight: bold;
+`;
